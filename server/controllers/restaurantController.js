43d4ec888349ed161162c33eb8e36fafd516761b
mailbau <muhammad.ismail.azmi@mail.ugm.ac.id>
2024-05-27 00:26:25 +0700

    fix: sequelize

--- server/controllers/restaurantController.js
+++ server/controllers/restaurantController.js
@@ -1,6 +1,7 @@
 const restaurant = require('../models/restaurantModel');
 const upload = require('../middleware/multerConfig');
 const favorite = require('../models/favoriteModel');
+const sequelize = require('../config/db');
 
 const restaurantController = {
     getAllRestaurants: async (req, res) => {
